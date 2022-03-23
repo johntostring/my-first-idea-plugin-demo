1f323498619b9bc82d45263aa6c3fe7d0835e27d
GitHub Action <action@github.com>
2022-03-23 07:05:57 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/johntostring/myfirstideaplugindemo/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.johntostring.myfirstideaplugindemo.services
+
+import com.github.johntostring.myfirstideaplugindemo.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
